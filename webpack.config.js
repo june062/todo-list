c2847224c8b40c7dbffd13c7280b725e39cbdb11
Juneid Teklu <te.juneid@gmail.com>
2024-07-10 14:43:07 -0500

    Additional environment setup

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,40 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  mode: 'development',
   entry: './src/index.js',
   devtool: 'inline-source-map',
   devServer: {
     static: './dist',
   },
+  devtool: "inline-source-map",
+  devServer: {
+    static: "./dist",
+    watchFiles: ['src/*.html',]
+  },
   output: {
-    filename: 'main.js',
+    filename: '[name].js',
     path: path.resolve(__dirname, 'dist'),
+    clean: true,
   },
   optimization: {
     runtimeChunk: 'single',
   },
+  module:{
+    rules: [
+        {
+            test: /\.css$/i,
+            use: ["style-loader", "css-loader"],
+        }
+    ]
+
+},
+  plugins:[
+    new HtmlWebpackPlugin({
+      title: "Todo",
+      template: "./src/index.html",
+      inject: 'head',
+    })
+  ]
 };
