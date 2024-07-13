b1ce7f9b4c174c4b04c58fcf3b6211b4d7b978fa
Juneid Teklu <te.juneid@gmail.com>
2024-07-13 15:05:27 -0500

    Create methods that add remove Project instances to and from arrayOfProjects

--- src/index.js
+++ src/index.js
@@ -2,22 +2,26 @@ import "./style.css"
 import {Project} from "./AppLogicModules/projects.js"
 
 
-/* let arrayOfProjects = [];
-function createProject(nameOfProject){
-    nameOfProject = new Project(`${nameOfProject}`)
-    arrayOfProjects.push(nameOfProject);
+ let arrayOfProjects = [];
+ 
+ let projectRemovalAndAddition = (function (){
 
-    function deleteProject(projectIndex){
-        this.arrayOfProjects.splice(projectIndex, 1);
+    function addProjectToArray(project){
+        arrayOfProjects.push(project);
     }
+    function removeProjectFromArray(projectIdentifier){
+        let projectIndex = arrayOfProjects.findIndex(element=>element.projectIdentifier === projectIdentifier);
+        arrayOfProjects.splice(projectIndex,1)
 
+    }
+    return {addProjectToArray, removeProjectFromArray}
 
-} */
+ })();
 
 let project1 = new Project("Project 1");
 console.log(project1);
-project1.createAndAddTask("task1", "That thing i have to do");
-project1.createAndAddTask("task2", "That other thing that I have to do")
+project1.createAndAddTask("task1");
+project1.createAndAddTask("task2", "That other thing that I have to do", "hi")
 
 let project2 = new Project("Project 2");
 project2.createAndAddTask("Thing for project 2",  "Damn I have to do this thing");
@@ -26,5 +30,13 @@ project2.createAndAddTask("third thing for project 2",  "Damn I also have to do 
 project2.createAndAddTask("forth thing for project 2",  "Damn I also have to do this thing")
 
 console.log(project2)
-project2.deleteTask(4); 
+
+
+projectRemovalAndAddition.addProjectToArray(project1);
+projectRemovalAndAddition.addProjectToArray(project2);
+
+projectRemovalAndAddition.removeProjectFromArray(0);
+console.log(arrayOfProjects)
+
+
 
